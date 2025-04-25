a7c0c39c2bf89a6852ce99722d3d60b4107ad3f7
braianbarraza <braian_019@hotmail.com>
2025-04-25 10:09:59 +0200

    vee-validate added, form and login schema created

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,55 @@
 <script setup>
+import { useForm, useField } from 'vee-validate'
+import { loginSchema as validationSchema } from '@/loginSchema.js'
+
+const { handleSubmit } = useForm({ validationSchema })
+const email = useField('email')
+const password = useField('password')
+
+const submit = handleSubmit(() =>{
+  console.log('submitting ...')
+})
+
 
 </script>
 
 <template>
-  <h1 class="text-center text-h3 font-weight-bold my-5">Login</h1>
+
+  <v-card
+    flat
+    max-width="600"
+    class="mx-auto my-10">
+    <v-card-title
+      class="text-h4 font-weight-bold"
+      tag="h3">
+      Login
+    </v-card-title>
+    <v-card-subtitle
+      class="text-h5">
+      Login with your account
+    </v-card-subtitle>
+
+    <v-form>
+      <v-text-field
+        type="email"
+        label="Email"
+        bg-color="blue-grey-lighten-5"
+        v-model="email.value.value" />
+
+      <v-text-field
+        type="password"
+        label="Password"
+        bg-color="blue-grey-lighten-5"
+        v-model="password.value.value" />
+
+      <v-btn
+        block
+        color="pink accent-3">
+        Login
+      </v-btn>
+
+    </v-form>
+  </v-card>
 
 </template>
 
